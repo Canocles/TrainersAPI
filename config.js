d40ebf7c5552c5a75016d22695cf71aaf8cdfe28
Canocles <rmm118@alu.ua.es>
2017-10-17 01:18:46 +0200

    Añadido mongoDB

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/trainnersAPI',
+    db: process.env.MONGODB_URI || 'mongodb://localhost:27017/trainnersAPI',
     SECRET_TOKEN: 'mitokenespracticamenteimposiblededesdifraren2017'
 }
